4cb3c4dbcef2da65e7f631a1d3bc67ff238f9b5d
ntexplorer <ntexplorer@qq.com>
2021-06-02 17:03:10 +0800

    S1W1C3 Half Finished 2. Code of S1W1C3 half way done;

---
+++ S1W1C3/src/LogicDemo.java
@@ -0,0 +1,14 @@
+/**
+ * @author Tian Zhang
+ */
+
+public class LogicDemo {
+    public static void main(String[] args) {
+        char ch = 'f';
+        if (ch >= 97 && ch <= 122) {
+            System.out.println("f是a到z之间的字符！");
+        } else {
+            System.out.println("f不是a到z之间的字符！");
+        }
+    }
+}
